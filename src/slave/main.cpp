009a6ee0f6aab6f9925cc94af24fd83651e34d7a
Adeel Asghar <adeel.asghar@liu.se>
2014-02-20 15:01:15 +0100

    Minor fixes in slave

--- src/slave/main.cpp
+++ src/slave/main.cpp
@@ -5,101 +5,129 @@
 #include <fmitcp/Server.h>
 #include <fmitcp/common.h>
 
+using namespace std;
 using namespace fmitcp;
 
-class MyFMIServer : public Server {
+// Define own server
+class FMIServer : public Server {
 public:
-    MyFMIServer(EventPump* pump) : Server(pump) {};
-    ~MyFMIServer(){};
+  FMIServer(string fmuPath, bool debugLogging, jm_log_level_enu_t logLevel, EventPump* pump) : Server(pump) {
+    setFmuPath(fmuPath);
+    setLogLevel(logLevel);
+  }
+  ~FMIServer() {};
+  void onClientConnect() {
+    //printf("MyFMIServer::onConnect\n");
+    //m_pump->exitEventLoop();
+  };
 
-    void onClientConnect(){
-        //printf("MyFMIServer::onConnect\n");
-        //m_pump->exitEventLoop();
-    };
+  void onClientDisconnect() {
+    //printf("MyFMIServer::onDisconnect\n");
+    //m_pump->exitEventLoop();
+  };
 
-    void onClientDisconnect(){
-        //printf("MyFMIServer::onDisconnect\n");
-        //m_pump->exitEventLoop();
-    };
-
-    void onError(string message){
-        printf("MyFMIServer::onError\n");fflush(NULL);
-        m_pump->exitEventLoop();
-    };
+  void onError(string message) {
+    printf("MyFMIServer::onError\n");fflush(NULL);
+    m_pump->exitEventLoop();
+  };
 };
 
-void printHelp(){
-    printf("HELP PAGE: TODO\n");fflush(NULL);
+void printHelp() {
+  printf("HELP PAGE: TODO\n");fflush(NULL);
 }
 
-int main( int argc, char *argv[] ) {
+int main(int argc, char *argv[]) {
 
-    printf("FMI Server %s\n",FMITCP_VERSION);
+  printf("FMI Server %s\n",FMITCP_VERSION);fflush(NULL);
 
-    if (argc == 1) {
-        // No args given, print help
-        printHelp();
-        return EXIT_SUCCESS;
-    }
-
-    int j;
-    long port = 3000;
-    bool loggingOn = false,
-         debugLogging = false,
-         debugFlag = false;
-    std::string hostName = "localhost",
-        fmuPath = "";
-
-    for (j = 1; j < argc; j++) {
-        std::string arg = argv[j];
-        bool last = (j==argc-1);
-
-        if (arg == "-h" || arg == "--help") {
-            printHelp();
-            return EXIT_SUCCESS;
-
-        } else if (arg == "-l" || arg == "--logging") {
-            loggingOn = true;
-
-        } else if (arg == "-d" || arg == "--debugLogging") {
-            debugLogging = true;
-
-        } else if (arg == "-v" || arg == "--version") {
-            printf("%s\n",FMITCP_VERSION); // todo
-            return EXIT_SUCCESS;
-
-        } else if (arg == "--debugFlag") {
-            debugFlag = true;
-
-        } else if((arg == "--port" || arg == "-p") && !last) {
-            std::string nextArg = argv[j+1];
-
-            std::istringstream ss(nextArg);
-            ss >> port;
-
-            if (port <= 0) {
-                printf("Invalid port.\n");fflush(NULL);
-                return EXIT_FAILURE;
-            }
-
-        } else if (arg == "--host" && !last) {
-            hostName = argv[j+1];
-
-        } else {
-            fmuPath = argv[j];
-        }
-    }
-
-    if(fmuPath == ""){
-        printHelp();
-        return EXIT_FAILURE;
-    }
-
-    EventPump pump;
-    MyFMIServer server(&pump);
-    server.host(hostName,port);
-    server.getLogger()->setFilter(Logger::LOG_NETWORK|Logger::LOG_DEBUG|Logger::LOG_ERROR);
-    pump.startEventLoop();
-
+  if (argc == 1) {
+    // No args given, print help
+    printHelp();
     return EXIT_SUCCESS;
+  }
+
+  int j;
+  long port = 3000;
+  bool debugLogging = false;
+  jm_log_level_enu_t log_level = jm_log_level_fatal;
+  int logging = jm_log_level_fatal;
+  string hostName = "localhost",
+      fmuPath = "";
+
+  for (j = 1; j < argc; j++) {
+    string arg = argv[j];
+    bool last = (j==argc-1);
+
+    if (arg == "-h" || arg == "--help") {
+      printHelp();
+      return EXIT_SUCCESS;
+
+    } else if (arg == "-d" || arg == "--debugLogging") {
+      debugLogging = true;
+
+    } else if (arg == "-v" || arg == "--version") {
+      printf("%s\n",FMITCP_VERSION); // todo
+      return EXIT_SUCCESS;
+
+    } else if ((arg == "-l" || arg == "--logging") && !last) {
+      std::string nextArg = argv[j+1];
+
+      std::istringstream ss(nextArg);
+      ss >> logging;
+
+      if (logging <= 0) {
+        printf("Invalid logging. Possible options are from 0 to 7.\n");fflush(NULL);
+        return EXIT_FAILURE;
+      }
+
+      switch (logging) {
+      case 0:
+        log_level = jm_log_level_nothing; break;
+      case 1:
+        log_level = jm_log_level_fatal; break;
+      case 2:
+        log_level = jm_log_level_error; break;
+      case 3:
+        log_level = jm_log_level_warning; break;
+      case 4:
+        log_level = jm_log_level_info; break;
+      case 5:
+        log_level = jm_log_level_verbose; break;
+      case 6:
+        log_level = jm_log_level_debug; break;
+      case 7:
+        log_level = jm_log_level_all; break;
+      }
+
+    } else if((arg == "--port" || arg == "-p") && !last) {
+      std::string nextArg = argv[j+1];
+
+      std::istringstream ss(nextArg);
+      ss >> port;
+
+      if (port <= 0) {
+        printf("Invalid port.\n");fflush(NULL);
+        return EXIT_FAILURE;
+      }
+
+    } else if (arg == "--host" && !last) {
+      hostName = argv[j+1];
+
+    } else {
+      fmuPath = argv[j];
+    }
+  }
+
+  if(fmuPath == ""){
+    printHelp();
+    return EXIT_FAILURE;
+  }
+
+  EventPump pump;
+  FMIServer server(fmuPath, debugLogging, log_level, &pump);
+  server.host(hostName, port);
+  server.getLogger()->setFilter(Logger::LOG_NETWORK | Logger::LOG_DEBUG | Logger::LOG_ERROR);
+  pump.startEventLoop();
+
+  return EXIT_SUCCESS;
 }
