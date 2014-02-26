85f2452ec5c19f161ad547d68994f44aefb1cc49
Stefan Hedman <schteppe@gmail.com>
2014-02-26 17:32:42 +0100

    updated help page

--- src/master/main.cpp
+++ src/master/main.cpp
@@ -13,7 +13,55 @@
 using namespace fmitcp_master;
 
 void printHelp(){
-    printf("HELP PAGE: TODO\n");fflush(NULL);
+    printf("Usage\n\
+\n\
+master [OPTIONS] [FMU_URLS...]\n\
+\n\
+OPTIONS\n\
+\n\
+    --port [INTEGER]\n\
+        The port to run the server on.\n\
+\n\
+    --host [STRING]\n\
+        The host name to run the server on.\n\
+\n\
+    --timeStep [NUMBER]\n\
+            Timestep size. Default is 0.1.\n\
+\n\
+    --stopAfter [NUMBER]\n\
+        End simulation time in seconds. Default is 1.0.\n\
+\n\
+    --weakMethod [STRING]\n\
+        Stepping  method for weak coupling connections. Must be \"parallel\" or \"serial\". Default is \"parallel\".\n\
+\n\
+    --weakConnections [STRING]\n\
+        Connection  specification. No connections by default. Quadruples of\n\
+        positive integers, representing which FMU and value reference to connect\n\
+        from and what to connect to. Syntax is\n\
+\n\
+            CONN1:CONN2:CONN3...\n\
+\n\
+        where CONNX is four comma-separated integers FMUFROM,VRFROM,FMUTO,VRTO.\n\
+        An example connection string is\n\
+\n\
+            0,0,1,0:0,1,1,1\n\
+\n\
+        which means: connect FMU0 (value reference 0) to FMU1 (vr 0) and FMU0\n\
+        (vr 1) to FMU1 (vr 1).  Default is no  connections.\n\
+\n\
+    --strongConnections [STRING]\n\
+        TODO\n\
+\n\
+    --help\n\
+        You're looking at it.\n\
+\n\
+FMU_URLS\n\
+\n\
+    Urls to FMU servers, separated by spaces. For example \"tcp://fmiserver.com:1234\".\n\
+\n\
+EXAMPLES\n\
+\n\
+    (TODO)\n\n");fflush(NULL);
 }
 
 int main(int argc, char *argv[] ) {
@@ -58,6 +106,7 @@ int main(int argc, char *argv[] ) {
 
             std::istringstream ss(nextArg);
             ss >> port;
+            j++;
 
             if (port <= 0) {
                 printf("Invalid port.\n");
@@ -100,19 +149,31 @@ int main(int argc, char *argv[] ) {
             std::string nextArg = argv[j+1];
             j++;
 
-            if (nextArg == "jacobi") {
-                //*method = jacobi;
+            if (nextArg == "parallel") {
+                master.setWeakMethod(PARALLEL);
 
-            } else if (nextArg == "jacobi") {
-                //*method = jacobi;
+            } else if (nextArg == "serial") {
+                master.setWeakMethod(SERIAL);
 
             } else {
-                fprintf(stderr,"Method not recognized. Use \"jacobi\" or \"gs\".\n");
+                fprintf(stderr,"Weak coupling method not recognized.\n");
                 return EXIT_FAILURE;
 
             }
 
         } else if ((arg == "-wc" || arg == "--weakConnections") && !last) {
+            std::string nextArg = argv[j+1];
+            j++;
+
+            // Get connections
+            vector<string> conns = split(nextArg,':');
+            for(i=0; i<conns.size(); i++){
+                vector<string> quad = split(conns[i],',');
+                weak_slaveA.push_back(string_to_int(quad[0]));
+                weak_slaveB.push_back(string_to_int(quad[1]));
+                weak_connA .push_back(string_to_int(quad[2]));
+                weak_connB .push_back(string_to_int(quad[3]));
+            }
 
         } else if ((arg == "-sc" || arg == "--strongConnections") && !last) {
             std::string nextArg = argv[j+1];
@@ -141,6 +202,12 @@ int main(int argc, char *argv[] ) {
     // Set connections
     //for(i=0; i<strong_slaveA.size(); i++)
     //    master.createStrongConnection(slaves[strong_slaveA[i]], slaves[strong_slaveB[i]], strong_connA[i], strong_connB[i]);
+
+    // Weak coupling
+    for(i=0; i<weak_slaveA.size(); i++){
+        printf("Creating weak connection %d %d %d %d\n",weak_slaveA[i],weak_slaveB[i],weak_connA[i],weak_connB[i]); fflush(NULL);
+        master.createWeakConnection(slaves[weak_slaveA[i]], slaves[weak_slaveB[i]], weak_connA[i], weak_connB[i]);
+    }
 
     master.simulate();
 
